bb0ae63b05d56c5e72d78d7577307ba4b8a957e2
Seetia Akpawu <seetia@hackthehood.org>
2021-06-21 11:17:13 -0700

    adding this file too

---
+++ HtH.py
@@ -0,0 +1,16 @@
+Food = {"Chicken":"$1.59", "Beef":"$1.99", "Cheese":"$1.00", "Milk":"$2.00"}
+Milk_price= Food ["Milk"]
+print(Milk_price) 
+
+Chicken_price= Food ["Chicken"]
+#print(Chicken_price) 
+
+Beef_price= Food ["Beef"]
+print(Beef_price) 
+
+Cheese_price= Food ["Cheese"]
+print(Cheese_price) 
+
+#NBA_players = {: 23, “Kevin Durant: 35, “Stephen Curry”: 30, “Damian Lillard”: 0}
+#jersey_number = NBA_players["lebron James"]
+#print(jersey_number)
